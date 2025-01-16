1e90f9143d9e9ae0d950389b3062fc2ba4424e4c
Harsh <harsh.joshi23@gmail.com>
2025-01-16 03:33:11 +0100

    Adding report as ipynb file

--- project/pipeline.py
+++ project/pipeline.py
@@ -6,7 +6,7 @@ import os
 
 def fetch_data(url):
     response = requests.get(url)
-    response.raise_for_status() 
+    response.raise_for_status()
     raw_data = pd.read_csv(io.StringIO(response.text))
     return raw_data
 
@@ -18,32 +18,37 @@ def clean_data(df, key_column):
     return cleaned_df
 
 def ensure_directory(path):
-    # Data directory exists
     os.makedirs(os.path.dirname(path), exist_ok=True)
 
 def store_data(df, db_name, table_name):
-    """ Store data into SQLite database """
     ensure_directory(db_name) 
     engine = create_engine(f'sqlite:///{db_name}')
     df.to_sql(table_name, con=engine, index=False, if_exists='replace')
 
 def main():
-    # URL
+    # URLs
     facilities_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/prisons/facilities.csv'
     us_data_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
     systems_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/prisons/systems.csv'
+    us_states_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/refs/heads/master/us-states.csv'
 
+    # Fetch data from URLs
     facilities_df = fetch_data(facilities_url)
     us_data_df = fetch_data(us_data_url)
     systems_df = fetch_data(systems_url)
+    us_states_df = fetch_data(us_states_url)
 
+    # Clean data
     facilities_df = clean_data(facilities_df, 'facility_name')
     us_data_df = clean_data(us_data_df, 'cases')  
-    systems_df = clean_data(systems_df, 'total_inmate_cases')  
+    systems_df = clean_data(systems_df, 'total_inmate_cases')
+    us_states_df = clean_data(us_states_df, 'state')  # Assuming 'state' is a key column
 
+    # Store data in SQLite database
     store_data(facilities_df, '../data/covid_data.sqlite', 'facilities')
     store_data(us_data_df, '../data/covid_data.sqlite', 'us_data')
     store_data(systems_df, '../data/covid_data.sqlite', 'systems')
+    store_data(us_states_df, '../data/covid_data.sqlite', 'us_states')
 
 if __name__ == "__main__":
     main()
